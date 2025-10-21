bb35306a49197026f025a37c0713cd22cf487095
David Eliason <davethemaker@gmail.com>
2025-10-21 13:40:13 -0700

    add button function to test creating a function and exporting it

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,8 +34,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <MyButton />
     </>
   )
 }
 
 export default App
+
